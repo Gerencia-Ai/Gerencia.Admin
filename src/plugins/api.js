2adf27d906a74356621659dac32685773e7feeb1
labs <gustavodep.gorges@gmail.com>
2023-11-17 10:57:59 -0300

    mudei url proj

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '../stores/auth'
 
 const api = axios.create({
-  baseURL: 'https://gerencia-back-dev-thtb.2.ie-1.fl0.io/api/'
+  baseURL: 'https://gerencia-back-dev-cxgk.4.us-1.fl0.io/api/'
 })
 
 api.interceptors.request.use((config) => {
